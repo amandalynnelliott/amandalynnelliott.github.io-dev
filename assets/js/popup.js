e13868112cec72b8d11fc5c9a87023f08e749a63
daviskauffmann <daviskauffmann@gmail.com>
2017-05-06 14:11:47 -0400

    Add image popups

---
+++ assets/js/popup.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    $('img').magnificPopup({
+        type: 'image'
+    });
+
+    /*
+        EXAMPLE
+        See: lightbox-WIP.md
+    */
+    $('.elephant-image').magnificPopup({
+        type: 'image',
+        items: [
+            {
+                src: '/assets/img/chart.png'
+            }
+        ]
+    });
+
+    /*
+        EXAMPLE
+        See: lightbox-WIP.md
+    */
+    $('.pplsketch-gallery').magnificPopup({
+        type: 'image',
+        items: [
+            {
+                src: '/assets/img/pplsketch.jpg'
+            },
+            {
+                src: '/assets/img/pplsketch2.jpg'
+            },
+            {
+                src: '/assets/img/pplsketch3.jpg'
+            }
+        ],
+        gallery: {
+            enabled: true
+        }
+    });
+
+    $('.elephant-gallery').magnificPopup({
+        type: 'image',
+        items: (function () {
+            var items = [];
+            for (var i = 1; i <= 24; i++) {
+                items.push({
+                    src: '/assets/img/2017/04/elephant/' + i + '.jpg'
+                });
+            }
+            return items;
+        })(),
+        gallery: {
+            enabled: true
+        }
+    });
+});
